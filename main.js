64f5bab648ba78d587c6d166edeede161bf4a2e2
geto7 <nikagetia193@gmail.com>
2025-06-07 21:30:43 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-fetch("matches.json")
-.then(res => res.json())
-.then(data => {
-    displayMatches(data);
-})
-.catch(error => console.error("მატჩები არ ჩაიტვირთა", error));
+fetch("https://apiv2.apifootball.com/?action=get_events&from=2025-06-01&to=2025-06-10&APIkey=d0e14b4ee1195e6e84a646a0a72817844d4930854aba5635e39d997fe601ede9")
+  .then(res => res.json())
+  .then(data => {
+      displayMatches(data);
+  })
+  .catch(error => console.error("მატჩები არ ჩაიტვირთა", error));
 
 function displayMatches(matches) {
-    const container =document.getElementById("matches");
+    const container = document.getElementById("matches");
 
     matches.forEach(match => {
         const matchElement = document.createElement("div");
         matchElement.classList.add("match");
 
         matchElement.innerHTML = `
-            <h2>${match.team1} vs ${match.team2}</h2>
-            <p>score: ${match.score}</p>
-            <p>date: ${match.date}</p>
-            <p>status: ${match.status}</p>
-            <a href="match.html?id=${match.id}">დეტალები</a>
+            <h2>${match.match_hometeam_name} vs ${match.match_awayteam_name}</h2>
+            <p>score: ${match.match_hometeam_score} - ${match.match_awayteam_score}</p>
+            <p>date: ${match.match_date}</p>
+            <p>status: ${match.match_status}</p>
         `;
 
         container.appendChild(matchElement);
--- register.js
+++ register.js
@@ -1,14 +1,20 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); 
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    
-    const user ={
+
+    if (!username || !password) {
+        alert("გთხოვ შეავსო ორივე ველი!");
+        return;
+    }
+
+    const user = {
         username: username,
         password: password
     };
 
     localStorage.setItem('user', JSON.stringify(user));
-    alert(" რეგისტრაცია წარმატებით დასრულდა!");
+    alert("რეგისტრაცია წარმატებით დასრულდა!");
     window.location.href = 'login.html';
-})
+});
